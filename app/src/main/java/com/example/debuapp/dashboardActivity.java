4ba4500311a5fa05d303d73d418172bad2f54b97
Amirkhan5949 <ak4729176@gmail.com>
2019-12-20 01:44:11 +0530

    Drawer Done...

--- app/src/main/java/com/example/debuapp/dashboardActivity.java
+++ app/src/main/java/com/example/debuapp/dashboardActivity.java
@@ -1,40 +1,97 @@
 package com.example.debuapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class dashboardActivity extends AppCompatActivity {
-    private Button logout;
-    private ImageView back;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+
+public class dashboardActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        logout=findViewById(R.id.logout);
-        back=findViewById(R.id.back);
 
-        back.setOnClickListener(new View.OnClickListener() {
+
+        drawerLayout=findViewById(R.id.drawerlayout);
+        navigationView=findViewById(R.id.navigation);
+
+        Log.i("sancbs", "onCreate: "+        drawerLayout);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (item.getItemId()){
+
+                    case R.id.chat:
+                        replace(new chatFragment());
+                        return true;
+
+                    case R.id.user:
+                        replace(new userFragment());
+                        return true;
+
+
+                    case R.id.logout:
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(dashboardActivity.this,loginActivity.class));
+                        finish();
+
+
+                    default:
+                        return false;
+
+
+                }
+
             }
         });
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(dashboardActivity.this,loginActivity.class));
-                finish();
-            }
-        });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void replace(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
+    }
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
 }
